a097d9ea60e70de4e1f43ec915f61d52da250207
Vin Halbwachs <vhalbwachs@gmail.com>
2017-12-09 14:01:56 -0500

    add books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+  return [
+    { title: 'Eat, Pray, Love' },
+    { title: `He's Just Not That Into You` },
+    { title: `It's Called a Breakup Because It's Broken` }
+  ]
+}
